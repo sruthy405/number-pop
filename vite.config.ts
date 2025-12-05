432647d09153ac1ca895025bf61a12d17590adfe
Sruthy Sojan <71270714+sruthy405@users.noreply.github.com>
2025-12-05 11:41:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
-
-export default defineConfig(() => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
 
-  plugins: [react()], 
+  base: "/number-pop/",
+
+  plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
